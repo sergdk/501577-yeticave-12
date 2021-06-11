d5356b049155382df5d7376b5c65388424bf5f5e
sergdk <sergpetrenko@ukr.net>
2021-06-11 20:40:58 +0300

    Незначительные правки

--- index.php
+++ index.php
@@ -6,28 +6,28 @@ $user_name =    'serg'; // укажите здесь ваше имя
 $categories =   [   
     [
         'title' => 'Доски и лыжи',
-        'code'  => ''
+        'code'  => '',
     ],
     [
         'title' => 'Крепления',
-        'code'  => ''
+        'code'  => '',
     ],
     [
         'title' => 'Ботинки',
-        'code'  => ''
+        'code'  => '',
     ],
     [
         'title' => 'Одежда',
-        'code'  => ''
+        'code'  => '',
     ],
     [
         'title' => 'Инструменты',
-        'code'  => ''
+        'code'  => '',
     ],
     [
         'title' => 'Разное',
-        'code'  => ''
-    ]
+        'code'  => '',
+    ],
 ];
 
 $items =    [
@@ -35,38 +35,38 @@ $items =    [
         'title' => '2014 Rossignol District Snowboard',
         'category' => 'Доски и лыжи',
         'price' =>    10999,
-        'url_img' => 'img/lot-1.jpg'
+        'url_img' => 'img/lot-1.jpg',
     ],
     [   
         'title' => 'DC Ply Mens 2016/2017 Snowboard',
         'category' => 'Доски и лыжи',
         'price' =>    159999,
-        'url_img' => 'img/lot-2.jpg'
+        'url_img' => 'img/lot-2.jpg',
     ],
     [   
         'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
         'category' => 'Крепления',
         'price' =>    8000,
-        'url_img' => 'img/lot-3.jpg'
+        'url_img' => 'img/lot-3.jpg',
     ],
     [   
         'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
         'category' => 'Ботинки',
         'price' =>    10999,
-        'url_img' => 'img/lot-4.jpg'
+        'url_img' => 'img/lot-4.jpg',
     ],
     [   
         'title' => 'Куртка для сноуборда DC Mutiny Charocal',
         'category' => 'Одежда',
         'price' =>    7533.123,
-        'url_img' => 'img/lot-5.jpg'
+        'url_img' => 'img/lot-5.jpg',
     ],
     [   
         'title' => 'Маска Oakley Canopy',
         'category' => 'Разное',
         'price' => 5400,
-        'url_img' => 'img/lot-6.jpg'
-    ]
+        'url_img' => 'img/lot-6.jpg',
+    ],
 ];
 
 function price_format($x) {
@@ -231,4 +231,5 @@ function price_format($x) {
 <script src="flatpickr.js"></script>
 <script src="script.js"></script>
 </body>
+
 </html>
